6e35b7532f194c7ecf71195e1b49cf546c3c95d8
CalvinCalluo <79105544+CalvinCalluo@users.noreply.github.com>
2022-02-05 18:35:20 -0500

    npm installed

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSk7aPyIvzmoHiXrxTKx4isPWcsF0_UOc",
+  authDomain: "reusability-ecefa.firebaseapp.com",
+  projectId: "reusability-ecefa",
+  storageBucket: "reusability-ecefa.appspot.com",
+  messagingSenderId: "837668182158",
+  appId: "1:837668182158:web:254534cb97170707e091df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

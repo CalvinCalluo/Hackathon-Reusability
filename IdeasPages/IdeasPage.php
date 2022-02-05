1b5ee5518736b87d97105d7fab9e1bb1b1c46ad2
Patrick He <77468352+phetheep@users.noreply.github.com>
2022-02-05 15:37:32 -0500

    Update IdeasPage.php

--- IdeasPages/IdeasPage.php
+++ IdeasPages/IdeasPage.php
@@ -51,7 +51,8 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 
   <!-- Header -->
   <header id="ideapage">
-    <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+    <a href="#ideapage">
+    <img src="/w3images/avatar_g2.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
     <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
     <div class="w3-container">
     <h1><b>Idea Page</b></h1>
@@ -127,7 +128,8 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
   </div>
 
   <div class="w3-container w3-padding-large" style="margin-bottom:32px">
-    <header id="ideapage">  
+    <header id="ideapage">
+    <a href="#ideapage">  
     <h4><b>Create Idea</b></h4>
     <p>Create Your Own Projects Here!</p>
     <hr>

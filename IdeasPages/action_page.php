b7b2b6454390010b73019d34e42440ac70c9d05e
CalvinCalluo <79105544+CalvinCalluo@users.noreply.github.com>
2022-02-05 15:03:49 -0500

    Update action_page.php

--- IdeasPages/action_page.php
+++ IdeasPages/action_page.php
@@ -1,5 +1,4 @@
 <?php 
-if(isset($_POST['submit'])){
     $to = "sustainabilityreusability@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
@@ -14,5 +13,4 @@ if(isset($_POST['submit'])){
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     header('Location: IdeasPage.php');
-    }
 ?>

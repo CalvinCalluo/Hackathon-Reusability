1b0bf43bb9f232f8ed89692185dcc1387d019d8e
CalvinCalluo <79105544+CalvinCalluo@users.noreply.github.com>
2022-02-05 18:39:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 

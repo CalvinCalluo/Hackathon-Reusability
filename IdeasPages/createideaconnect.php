67020b44d9b45ad4ac922df397e42bbb28e7f1f9
Patrick He <77468352+phetheep@users.noreply.github.com>
2022-02-05 22:55:26 -0500

    nj

---
+++ IdeasPages/createideaconnect.php
@@ -0,0 +1,40 @@
+<?php
+$db = parse_url(getenv("host=ec2-52-200-155-213.compute-1.amazonaws.com;port=5432;user=jxfpzqqmboccjr;password=193dcc4b2bc8d7048a65e63b33a6e66fd39151f9810fd29083b9f7d020694480;dbname=d2ulfua00e8ep4"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=ec2-52-200-155-213.compute-1.amazonaws.com;port=5432;user=jxfpzqqmboccjr;password=193dcc4b2bc8d7048a65e63b33a6e66fd39151f9810fd29083b9f7d020694480;dbname=d2ulfua00e8ep4",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+$title = $_POST["Title"];
+$description = $_POST["Description"];
+$materials = $_POST["Materials"];
+$filename = $_POST["Filename"];
+
+$sql = $pdo->query(INSERT INTO ideainfo (Title, Description, Materials, Image) VALUES ($title, $description, $materials, $filename));
+
+/*
+$result = $pdo->query('SELECT username, password FROM logininfo');
+if ($result->rowCount() > 0){
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    //echo $email.' '.$row['username'];
+    //echo $passcode.' '.$row['password'];
+    if($row['username'] == $email && $row['password'] == $passcode){
+      // Correct username and password, logged in	
+      header('Location: IdeasPages/IdeasPage.php');
+      die();
+        
+      echo 'correct';
+    }
+      else {
+      // Incorrect password
+      //$_SESSION["error"] = 'Incorrect username and/or password!';
+      //header('Location: loginPage.php');
+      echo 'incorrect';
+    }
+  } 
+?>

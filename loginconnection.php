3f31c52bc56669d0b46e92fd05200935eefda853
CalvinCalluo <79105544+CalvinCalluo@users.noreply.github.com>
2022-02-06 08:29:32 -0500

    Update loginconnection.php

--- loginconnection.php
+++ loginconnection.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$_SESSION["loggedIn"] = false;
 $db = parse_url(getenv("host=ec2-52-200-155-213.compute-1.amazonaws.com;port=5432;user=jxfpzqqmboccjr;password=193dcc4b2bc8d7048a65e63b33a6e66fd39151f9810fd29083b9f7d020694480;dbname=d2ulfua00e8ep4"));
 
 $pdo = new PDO("pgsql:" . sprintf(
@@ -16,7 +15,7 @@ $email = $_POST["Username/Email"];
 $passcode = $_POST["Password"];
 //echo 'email: '.$email.'\n';
 //echo 'password: '.$passcode.'\n';
-if($_SESSION["loggedIn"] == false){
+if(!isset($_SESSION["loggedIn"])){
   $result = $pdo->query('SELECT username, password FROM logininfo');
   if ($result->rowCount() > 0){
     $row = $result->fetch(PDO::FETCH_ASSOC);

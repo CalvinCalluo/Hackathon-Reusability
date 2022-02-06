6a3ea92be2296eaaa1c361f7137647a9fffbc4b3
CalvinCalluo <79105544+CalvinCalluo@users.noreply.github.com>
2022-02-06 08:27:05 -0500

    dsa

--- loginconnection.php
+++ loginconnection.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$_SESSION["loggedIn"] = false;
 $db = parse_url(getenv("host=ec2-52-200-155-213.compute-1.amazonaws.com;port=5432;user=jxfpzqqmboccjr;password=193dcc4b2bc8d7048a65e63b33a6e66fd39151f9810fd29083b9f7d020694480;dbname=d2ulfua00e8ep4"));
 
 $pdo = new PDO("pgsql:" . sprintf(
@@ -14,15 +16,16 @@ $email = $_POST["Username/Email"];
 $passcode = $_POST["Password"];
 //echo 'email: '.$email.'\n';
 //echo 'password: '.$passcode.'\n';
-
-$result = $pdo->query('SELECT username, password FROM logininfo');
-if ($result->rowCount() > 0){
+if($_SESSION["loggedIn"] == false){
+  $result = $pdo->query('SELECT username, password FROM logininfo');
+  if ($result->rowCount() > 0){
     $row = $result->fetch(PDO::FETCH_ASSOC);
     //echo $email.' '.$row['username'];
     //echo $passcode.' '.$row['password'];
     if($row['username'] == $email && $row['password'] == $passcode){
       // Correct username and password, logged in	
       header('Location: IdeasPages/IdeasPage.php');
+      $_SESSION["loggedIn"] = true;
       die();
         
       echo 'correct';
@@ -30,8 +33,13 @@ if ($result->rowCount() > 0){
       else {
       // Incorrect password
       //$_SESSION["error"] = 'Incorrect username and/or password!';
-      //header('Location: loginPage.php');
+      header('Location: loginPage.html');
       echo 'incorrect';
     }
   } 
+}
+else{
+  header('Location: ideasPage.php');
+}
+
 ?>

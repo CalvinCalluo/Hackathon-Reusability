cbd29a94bd77e6c7eeee8ae24c5b3a324d59ded8
Patrick He <77468352+phetheep@users.noreply.github.com>
2022-02-05 21:48:12 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 $db = parse_url(getenv("ec2-52-200-155-213.compute-1.amazonaws.com"));
 
-$pdo = new PDO("pgsql:" . sprintf(
+$pdo = new mysqli("pgsql:" . sprintf(
     "host=ec2-52-200-155-213.compute-1.amazonaws.com;port=5432;user=jxfpzqqmboccjr;password=193dcc4b2bc8d7048a65e63b33a6e66fd39151f9810fd29083b9f7d020694480;dbname=d2ulfua00e8ep4",
     $db["host"],
     $db["port"],

9e8acc191f22fa4b62b9918e1a3f29b9df4c62aa
Patrick He <77468352+phetheep@users.noreply.github.com>
2022-02-05 21:57:32 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,19 +1,19 @@
 <?php
 $db = parse_url(getenv("ec2-52-200-155-213.compute-1.amazonaws.com"));
 
-$pdo = new mysqli("pgsql:" . sprintf(
+$pdo = new PDO("pgsql:" . sprintf(
     "host=ec2-52-200-155-213.compute-1.amazonaws.com;port=5432;user=jxfpzqqmboccjr;password=193dcc4b2bc8d7048a65e63b33a6e66fd39151f9810fd29083b9f7d020694480;dbname=d2ulfua00e8ep4",
     $db["host"],
     $db["port"],
     $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
+    $db["password"],
+    ltrim($db["dbname"], "/")
 ));
 
 $email = $_POST["email"];
 $password = $_POST["password"];
 $u = 'SELECT username, passcode FROM logininfo';
-$result = mysqli_query($pdo, $u);
+$result = $pdo->query($pdo, $u);
 if (mysqli_num_rows($result) > 0){
     $row = mysqli_fetch_assoc($result);
     if($row['username'] == $email && $row['passcode'] == $password){
